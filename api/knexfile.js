d830619d58c5c1f152cac59400523121795d5f10
chapi <chapi@ip-10-232-37-160.ec2.internal>
2023-03-21 10:16:29 -0400

    updated db cred

--- api/knexfile.js
+++ api/knexfile.js
@@ -1,31 +1,31 @@
 export default {
   development: {
-    client: 'postgresql',
+    client: 'pg',
     connection: {
-      host: process.env.DEV_DB_HOST || 'db',
-      database: process.env.DEV_DB_NAME || 'hitech_apd',
-      port: process.env.DEV_DB_PORT || 5432,
-      user: process.env.DEV_DB_USER || 'postgres',
-      password: process.env.DEV_DB_PASSWORD || 'cms'
+      host: 'eapd-dev.ci3dq79po163.us-east-1.rds.amazonaws.com',
+      database: 'hitech_apd',
+      port: 5432,
+      user: 'postgres',
+      password: ']*LT>O#N>5I&och*'
     },
     seeds: { directory: './seeds/development' }
-  },
-
-  test: {
-    client: 'postgresql',
-    connection: {
-      host: process.env.TEST_DB_HOST || 'db',
-      database: process.env.TEST_DB_NAME || 'hitech_apd_test',
-      port: process.env.TEST_DB_PORT || 5432,
-      user: process.env.TEST_DB_USER || 'postgres',
-      password: process.env.TEST_DB_PASSWORD || 'cms'
-    },
-    seeds: { directory: './seeds/test' }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: process.env.DATABASE_URL,
-    seeds: { directory: './seeds/production' }
   }
+
+  //   test: {
+  //     client: 'postgresql',
+  //     connection: {
+  //       host: process.env.TEST_DB_HOST || 'db',
+  //       database: process.env.TEST_DB_NAME || 'hitech_apd_test',
+  //       port: process.env.TEST_DB_PORT || 5432,
+  //       user: process.env.TEST_DB_USER || 'postgres',
+  //       password: process.env.TEST_DB_PASSWORD || 'cms'
+  //     },
+  //     seeds: { directory: './seeds/test' }
+  //   },
+
+  //   production: {
+  //     client: 'postgresql',
+  //     connection: process.env.DATABASE_URL,
+  //     seeds: { directory: './seeds/production' }
+  //   }
 };
