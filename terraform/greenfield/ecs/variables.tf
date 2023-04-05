7d657a1fbcefa3dd13b3ffe32ed395b0bf296e8b
chapi <chapi@ip-10-232-46-78.ec2.internal>
2023-04-05 12:25:38 -0400

    updated affliiations api path

--- terraform/greenfield/ecs/variables.tf
+++ terraform/greenfield/ecs/variables.tf
@@ -52,13 +52,13 @@ variable "private_subnets" {
 
 # Docker Images
 variable "web_image" {
-  default     = "894719201277.dkr.ecr.us-east-1.amazonaws.com/eapd-dev-web:ca3689c" 
+  default     = "894719201277.dkr.ecr.us-east-1.amazonaws.com/eapd-dev-web:41a9bd8" 
   description = "Docker image name with tag for web"
   type        = string
 }
 
 variable "api_image" {
-  default     = "894719201277.dkr.ecr.us-east-1.amazonaws.com/eapd-dev-api:ca3689c" 
+  default     = "894719201277.dkr.ecr.us-east-1.amazonaws.com/eapd-dev-api:41a9bd8" 
   description = "Docker image name with tag for api"
   type        = string
 }
